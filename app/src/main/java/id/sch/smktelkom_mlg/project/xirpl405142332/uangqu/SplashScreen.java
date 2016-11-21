819945dc2f148a2e4a1c6b1db883b2b2a811e161
Faiza Aulia Rahma Putra <faizaaulia20@gmail.com>
2016-11-21 23:57:50 +0700

    Splash Screen

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/SplashScreen.java
@@ -0,0 +1,41 @@
+package id.sch.smktelkom_mlg.project.xirpl405142332.uangqu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //set waktu splash screen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i); // menghubungkan activity splashscren ke main activity dengan intent
+                //jeda selesai splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+            }
+        }, splashInterval);
+    }
+}
