5eee1f99fae28119ef8b8ca990a764f069ef76eb
anindita88 <anindita.ekasuci88@gmail.com>
2016-11-22 00:21:32 +0700

    +Update 1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/UpdateBiodata.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl405142332/uangqu/UpdateBiodata.java
@@ -1,8 +1,20 @@
 package id.sch.smktelkom_mlg.project.xirpl405142332.uangqu;
 
-/**
- * Created by Ridjal Fathoni on 22/11/2016.
- */
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UpdateBiodata {
+public class UpdateBiodata extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    Button ton1, ton2;
+    EditText text1, text2, text3, text4, text5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_biodata);
+    }
 }
